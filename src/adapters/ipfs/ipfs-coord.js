d6e0e7b9031bddcc75a977170571cbaefb94ecd6
Chris Troutner <chris.troutner@gmail.com>
2023-10-22 07:51:45 -0700

    got helia working

--- src/adapters/ipfs/ipfs-coord.js
+++ src/adapters/ipfs/ipfs-coord.js
@@ -5,7 +5,8 @@
 */
 
 // Global npm libraries
-import IpfsCoord from 'ipfs-coord-esm'
+// import IpfsCoord from 'ipfs-coord-esm'
+import IpfsCoord from 'helia-coord'
 
 // import BCHJS from '@psf/bch-js';
 import SlpWallet from 'minimal-slp-wallet'
@@ -64,12 +65,15 @@ class IpfsCoordAdapter {
       }
     }
 
+    const nullLog = () => {}
+
     const ipfsCoordOptions = {
       ipfs: this.ipfs,
       type: 'node.js',
       // type: 'browser',
       wallet: this.wallet,
-      privateLog: console.log, // Default to console.log
+      // privateLog: console.log, // Default to console.log
+      privateLog: nullLog,
       isCircuitRelay: this.config.isCircuitRelay,
       circuitRelayInfo,
       apiInfo: this.config.apiInfo,
@@ -78,9 +82,9 @@ class IpfsCoordAdapter {
     }
 
     // Production env uses external go-ipfs node.
-    if (this.config.isProduction) {
-      ipfsCoordOptions.nodeType = 'external'
-    }
+    // if (this.config.isProduction) {
+    //   ipfsCoordOptions.nodeType = 'external'
+    // }
 
     this.ipfsCoord = new this.IpfsCoord(ipfsCoordOptions)
 
--- src/adapters/ipfs/ipfs.js
+++ src/adapters/ipfs/ipfs.js
@@ -13,9 +13,17 @@
 // const IPFS = require('@chris.troutner/ipfs')
 // import IPFSembedded from 'ipfs';
 
-import { create } from 'ipfs-http-client'
+import { createHelia } from 'helia'
 import fs from 'fs'
-import http from 'http'
+import { FsBlockstore } from 'blockstore-fs'
+import { FsDatastore } from 'datastore-fs'
+import { createLibp2p } from 'libp2p'
+import { tcp } from '@libp2p/tcp'
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { bootstrap } from '@libp2p/bootstrap'
+import { identifyService } from 'libp2p/identify'
+import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 
 // Local libraries
 import config from '../../../config/index.js'
@@ -27,7 +35,7 @@ class IpfsAdapter {
     // Encapsulate dependencies
     this.config = config
     this.fs = fs
-    this.create = create
+    // this.create = create
 
     // Choose the IPFS constructor based on the config settings.
     // this.IPFS = IPFSembedded // default
@@ -42,61 +50,22 @@ class IpfsAdapter {
   // Start an IPFS node.
   async start () {
     try {
-      // Ipfs Options
-      const ipfsOptionsEmbedded = {
-        repo: IPFS_DIR,
-        start: true,
-        config: {
-          relay: {
-            enabled: true, // enable circuit relay dialer and listener
-            hop: {
-              enabled: config.isCircuitRelay // enable circuit relay HOP (make this node a relay)
-            }
-          },
-          pubsub: true, // enable pubsub
-          Swarm: {
-            ConnMgr: {
-              HighWater: 30,
-              LowWater: 10
-            }
-          },
-          Addresses: {
-            Swarm: [
-              `/ip4/0.0.0.0/tcp/${this.config.ipfsTcpPort}`,
-              `/ip4/0.0.0.0/tcp/${this.config.ipfsWsPort}/ws`
-            ]
-          },
-          Datastore: {
-            StorageMax: '2GB',
-            StorageGCWatermark: 50,
-            GCPeriod: '15m'
-          }
-        }
-      }
+      // Create an IPFS node
+      const ipfs = await this.createNode()
+      // console.log('ipfs: ', ipfs)
 
-      const ipfsOptionsExternal = {
-        host: this.config.ipfsHost,
-        port: this.config.ipfsApiPort,
-        agent: http.Agent({ keepAlive: true, maxSockets: 2000 })
-      }
+      // Get the multiaddrs for the node.
+      const multiaddrs = ipfs.libp2p.getMultiaddrs()
+      console.log('Multiaddrs: ', multiaddrs)
 
-      let ipfsOptions = ipfsOptionsEmbedded
-      if (this.config.isProduction) {
-        ipfsOptions = ipfsOptionsExternal
-      }
-
-      // Create a new IPFS node.
-      this.ipfs = await this.create(ipfsOptions)
-
-      // Set the 'server' profile so the node does not scan private networks.
-      await this.ipfs.config.profiles.apply('server')
-
-      // Debugging: Display IPFS config settings.
-      // const configSettings = await this.ipfs.config.getAll()
-      // console.log(`configSettings: ${JSON.stringify(configSettings, null, 2)}`)
+      this.multiaddrs = multiaddrs
+      this.id = ipfs.libp2p.peerId.toString()
+      console.log('IPFS ID: ', this.id)
 
       // Signal that this adapter is ready.
       this.isReady = true
+
+      this.ipfs = ipfs
 
       return this.ipfs
     } catch (err) {
@@ -108,6 +77,69 @@ class IpfsAdapter {
       // }
 
       throw err
+    }
+  }
+
+  // This function creates an IPFS node using Helia.
+  // It returns the node as an object.
+  async createNode () {
+    try {
+
+      // Create block and data stores.
+      const blockstore = new FsBlockstore(`${IPFS_DIR}/blockstore`)
+      const datastore = new FsDatastore(`${IPFS_DIR}/datastore`)
+
+      // libp2p is the networking layer that underpins Helia
+      const libp2p = await createLibp2p({
+        datastore,
+        addresses: {
+          listen: [
+            '/ip4/127.0.0.1/tcp/0',
+            `/ip4/0.0.0.0/tcp/${this.config.ipfsTcpPort}`
+          ]
+        },
+        transports: [
+          tcp()
+        ],
+        connectionEncryption: [
+          noise()
+        ],
+        streamMuxers: [
+          yamux()
+        ],
+        peerDiscovery: [
+          bootstrap({
+            list: [
+              // '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+              // '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+              // '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+              // '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+              '/ip4/5.161.108.190/tcp/4102/p2p/12D3KooWKNuBjaMgEDN2tGqzmdfM2bmd22VEuboC4X7x8ua4DvUg',
+              '/ip4/5.161.108.190/tcp/4001/p2p/12D3KooWMW3u8ygGHqNHjxXsw7TA39vgrSQEx7vEGoUh7EiCwGGv',
+              '/ip4/137.184.13.92/tcp/4001/p2p/12D3KooWMbpo92kSGwWiN6QH7fvstMWiLZKcxmReq3Hhbpya2bq4',
+              '/ip4/78.46.129.7/tcp/4001/p2p/12D3KooWJyc54njjeZGbLew4D8u1ghrmZTTPyh3QpBF7dxtd3zGY',
+              '/ip4/5.161.72.148/tcp/4001/p2p/12D3KooWAVG5kn46P9mjKCLUSPesmmLeKf2a79qRt6u22hJaEARJ',
+              '/ip4/161.35.99.207/tcp/4001/p2p/12D3KooWDtj9cfj1SKuLbDNKvKRKSsGN8qivq9M8CYpLPDpcD5pu',
+              '/ip4/192.168.2.173/tcp/4001/p2p/12D3KooWAz2hbriyU9o1wHNU2nxqG8mMgAdWnXCSfWiv5kxSfW49'
+            ]
+          })
+        ],
+        services: {
+          identify: identifyService(),
+          pubsub: gossipsub({ allowPublishToZeroPeers: true })
+        }
+      })
+
+      // create a Helia node
+      const helia = await createHelia({
+        blockstore,
+        datastore,
+        libp2p
+      })
+
+      return helia
+    } catch (err) {
+      console.error('Error creating Helia node: ', err)
     }
   }
 
