e5cdf28e34ee32eba200366bfa3112cdba8eafc7
Chris Troutner <chris.troutner@gmail.com>
2025-02-20 19:03:45 -0700

    Catching and reporting AbortError race condition

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import Server from './bin/server.js'
 const server = new Server()
 
+process.on('unhandledRejection', (reason, promise) => {
+  console.log(`Handling ${reason.code} error. stack: `, reason)
+})
+
 server.startServer()
