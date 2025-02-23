f1caef2c0df3aca12e2dd14897dc7248da767570
Chris Troutner <chris.troutner@gmail.com>
2025-02-23 09:41:55 -0700

    fix(prod): Adding connection prefrence to production startup script

--- production/docker/start-production.sh
+++ production/docker/start-production.sh
@@ -36,4 +36,9 @@ export IPFS_WS_PORT=4003
 # 0 = no debug logs. 3 = maximum debug logs.
 export DEBUG_LEVEL=0
 
+# Use this if the IPFS node has a publically accessible IP address.
+#export CONNECT_PREF=direct
+# Use this if the IPFS node is behind a NAT or Firewall and cannot be accessed directly.
+export CONNECT_PREF=cr
+
 npm start
